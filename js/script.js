c855b48d0566f685fee1ade83460d869e3571dba
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2018-01-26 00:19:54 +0600

    final

---
+++ js/script.js
@@ -0,0 +1,74 @@
+/*global var1,var2,var3,var4,var5*/
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+
+$(document).ready(function () {
+    
+    /* Sticky Navigation */
+    $('.project').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('nav__bar');
+        } else {
+            $('nav').removeClass('nav__bar');
+        }
+    }, {
+        // offset: '5%'
+    });
+
+
+
+
+
+
+    // Select all links with hashes
+// $('a[href*="#"]')
+// // Remove links that don't actually link to anything
+// .not('[href="#"]')
+// .not('[href="#0"]')
+// .click(function(event) {
+//   // On-page links
+//   if (
+//     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+//     && 
+//     location.hostname == this.hostname
+//   ) {
+//     // Figure out element to scroll to
+//     var target = $(this.hash);
+//     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//     // Does a scroll target exist?
+//     if (target.length) {
+//       // Only prevent default if animation is actually gonna happen
+//       event.preventDefault();
+//       $('html, body').animate({
+//         scrollTop: target.offset().top
+//       }, 500, function() {
+//         // Callback after animation
+//         // Must change focus!
+//         var $target = $(target);
+//         $target.focus();
+//         if ($target.is(":focus")) { // Checking if the target was focused
+//           return false;
+//         } else {
+//           $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+//           $target.focus(); // Set focus again
+//         };
+//       });
+//     }
+//   }
+// });
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
